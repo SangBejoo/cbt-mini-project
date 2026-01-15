c38f20048e108204d9f86a4a9cdb98ee91ac6812
Ayub Subagiya <ayub.subagiya.tik21@mhsw.pnj.ac.id>
2026-01-16 04:59:51 +0700

    Remove web folder from backend repo

--- web/src/app/admin/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './useCRUD';
-export * from './useForm';
-export * from './usePagination';
-export { useUsers } from './useUsers';
-export { useSessions } from './useSessions';
-export { useQuestions } from './useQuestions';
